d2eff9bff61abb9a988c3e5b627874147d3d7ece
Mark OBrien <markob83@yahoo.com>
2015-07-06 04:46:05 -0500

    first commit

---
+++ web/laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 7/5/2015
+ * Time: 2:13 AM
+ */
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller{
+
+}
