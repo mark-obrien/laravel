0687b18d11f5812f121c03bd7ab522052b8e67d3
Mark OBrien <markob83@yahoo.com>
2015-07-12 11:52:16 -0500

    pages controller

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('about', 'PagesController@about');
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('contact', 'PagesController@contact');
---
+++ web/resources/views/pages/about.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+
+@section('content')
+
+    <h1>About</h1>
+
+    <h3>People</h3>
+
+    @if(count($people))
+        <ul>
+            @foreach($people as $person)
+                <li>{{$person}}</li>
+            @endforeach
+        </ul>
+    @endif
+
+@stop
