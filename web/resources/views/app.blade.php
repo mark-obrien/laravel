dd7047a38e797f605e43127531b9abadcf3d113c
Mark OBrien <mark@Marks-MacBook-Pro.local>
2015-08-12 21:28:59 -0500

    flash messages

--- web/resources/views/app.blade.php
+++ web/resources/views/app.blade.php
@@ -54,6 +54,7 @@
 	</nav>
 
     <div class="container">
+        @include('flash::message')
         @yield('content')
     </div>
 
@@ -62,5 +63,10 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+        $('#flash-overlay-modal').modal();
+    </script>
+
 </body>
 </html>
