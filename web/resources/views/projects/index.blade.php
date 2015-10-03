c06b027c97e97d7063f67bbdee9fa02728a5edd8
Mark OBrien <mark.obrien@blueearth.net>
2015-10-02 22:23:59 -0500

    add project check

--- web/resources/views/projects/index.blade.php
+++ web/resources/views/projects/index.blade.php
@@ -5,10 +5,13 @@
 @stop
 
 @section('content')
+    @if($projects)
     @foreach($projects as $project)
         <article>
             <h2><a href="{{ action('ProjectsController@show', [ $project->id]) }}">{{ $project->name }}</a></h2>
             <div class="article-body">{{ $project->name }}</div>
         </article>
     @endforeach
+    @else
+
 @stop
